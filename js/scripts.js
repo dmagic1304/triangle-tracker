e9f94f6061dcddc614672babf9b3d1f2f837c4fc
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-20 14:32:42 -0700

    It will add header to index and pseudocode for js.
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+/*
+Given tri formula
+- if/else for give tri-type 
+equilateral - all sides = input1,i2,i3, 
+isosceles - 2 sides ===, or 
+scalene - no === sides triangle, or 
+if the sides cannot make a triangle at all
+if a+b <=c || a + c <= b || b + c <= a
+*/
+
 // BI
 
 // UI
@@ -5,3 +15,5 @@
 const numberOne = document.getElementById("number-one").value;
 const numberTwo = document.getElementById("number-two").value;
 const numberThree = document.getElementById("number-three").value;
+
+
