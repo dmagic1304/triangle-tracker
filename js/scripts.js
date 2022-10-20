a7059fce1b494f06bc0a4d4240029232ff613fc6
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-20 14:17:13 -0700

    It will add label input setups to html and number const defs to the hs.
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// BI
+
+// UI
+
+const numberOne = document.getElementById("number-one").value;
+const numberTwo = document.getElementById("number-two").value;
+const numberThree = document.getElementById("number-three").value;
